f20b94da48ea5b476f6cd05112ec21264170be61
fabihanadeem528-source <fabihanadeem528@gmail.com>
2026-02-21 14:11:19 +0500

    commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// var studentname = "Fabiha"
+// studentcourse
+//)
+// chapter No O1
+// Question # 01
+
+alert("Hello Everyone.");
+
+// Chapter no 01
+// Question # 02
+
+document.write("Hello Everyone.")
+
+// Question # 03
+alert("Hello Everyone")
+
+// Question # 04
+alert("wELCOME TO JS land");
+
+alert("Happy coding");
+
+// Question # 05
+alert("Heloo... I can run js through my web browser's console ")
+console.log("Heloo... I can run js through my web browser's console")
+    
