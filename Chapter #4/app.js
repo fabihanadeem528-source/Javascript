100a6a3546f5b56512161bbc544bebbfdd11b8be
fabihanadeem528-source <fabihanadeem528@gmail.com>
2026-03-08 12:23:45 +0500

    complet

---
+++ Chapter #4/app.js
@@ -0,0 +1,28 @@
+// Q #1
+
+// let userName , userMail , userAge
+
+// Q #2
+
+// legal verialbes
+// let legal , $legal , _legal , legal_name , legal1
+
+// illegal variable
+// let 1illegal , @illegal , illegal name , illegal-student , *illegal
+
+// Q #3
+
+// a)
+// document.write("Rules for naming JS variables")
+
+// b)
+// let $my_1stVariable
+
+// c)
+// let $Fabiha , _Fabiha , Fabiha
+
+// d)
+// sensitive
+
+// e)
+// keywords
---
+++ chapter #2/app.js
@@ -0,0 +1,45 @@
+// Q #1
+let  userName = "Fabiha Nadeem";
+
+// Q #2
+let myName = "Fabiha Nadeem";
+
+// Q #3
+
+// a)
+let message
+
+// b)
+ message = "Hello world"
+
+//  c)
+alert(message)
+
+// Q #4
+
+let studentName = "jhone Doe"
+let studentAge =  15 + "Years old"
+let studentEducation = "Certificate Mobile Application Development"
+
+alert(studentName)
+alert(studentAge)
+alert(studentEducation)
+// Q #5
+let pizza = ""
+alert("PIZZA\n PIZZ\n PIZ\n PI\n P\n")
+alert (pizza)
+
+// Q #6
+let email = "fabihanadeem69@gmail.com"
+alert("my email adress is" + email)
+
+// Q #7
+let book = "A smarter way to learn Javascript."
+alert("I am traying to learn form the book" + book)
+
+// Q #8
+document.write("Yah! I can write HTML content through javascript")
+
+// Q #9
+// let design = “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+// alert (design)
